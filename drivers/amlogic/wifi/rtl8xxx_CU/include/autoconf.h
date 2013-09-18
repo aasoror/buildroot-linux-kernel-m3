6d3e6fb81be3f983b968550015c0ef04bc263341
Stanislav Vlasic <svlasic@gmail.com>
2013-09-18 10:00:59 +0200

    Apply 2.6.34.14 patch

--- drivers/amlogic/wifi/rtl8xxx_CU/include/autoconf.h
+++ drivers/amlogic/wifi/rtl8xxx_CU/include/autoconf.h
@@ -32,7 +32,7 @@
 #define PLATFORM_LINUX	1
 
 
-#define CONFIG_IOCTL_CFG80211 1
+//#define CONFIG_IOCTL_CFG80211 1
 #ifdef CONFIG_IOCTL_CFG80211
 	#define CONFIG_CFG80211_FORCE_COMPATIBLE_2_6_37_UNDER
 	//#define CONFIG_DEBUG_CFG80211 1
@@ -69,7 +69,7 @@
 	#define SUPPORT_HW_RFOFF_DETECTED	1
 	
 	#define CONFIG_LPS	1
-	#define CONFIG_BT_COEXIST	1
+	#define CONFIG_BT_COEXIST	0
 	//befor link
 	#define CONFIG_ANTENNA_DIVERSITY	 	
 	//after link
@@ -168,7 +168,7 @@
 #endif
 
 #define CONFIG_PREALLOC_RECV_SKB	1
-//#define CONFIG_REDUCE_USB_TX_INT	1	// Trade-off: Improve performance, but may cause TX URBs blocked by USB Host/Bus driver on few platforms.
+#define CONFIG_REDUCE_USB_TX_INT	1	// Trade-off: Improve performance, but may cause TX URBs blocked by USB Host/Bus driver on few platforms.
 //#define CONFIG_EASY_REPLACEMENT	1
 
 /* 
@@ -253,7 +253,7 @@
 //#define CONFIG_DEBUG_RTL871X
 
 #define DBG	0
-#define CONFIG_DEBUG_RTL819X
+//#define CONFIG_DEBUG_RTL819X
 
 #define CONFIG_PROC_DEBUG	1
 
